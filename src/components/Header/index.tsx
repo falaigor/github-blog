e92f3e1f1ac05819bc1334ef4a52c81c95a2f67d
Igor Santos <falaigors@gmail.com>
2022-10-19 13:20:00 -0300

    create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import {HeaderContainer, HeaderWrapper} from "./styles";
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <HeaderWrapper>
+                <img src="/logo.svg" alt="Logo Github Blog" />
+            </HeaderWrapper>
+        </HeaderContainer>
+    )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+background-image: url(/cover.svg);
+background-size: cover;
+background-color: ${props => props.theme["base-profile"]}
+`
+
+export const HeaderWrapper = styled.div`
+display:flex;
+justify-content:center;
+align-items:center;
+
+img {
+padding-top: 4rem;
+padding-bottom: 8rem;
+}
+`
