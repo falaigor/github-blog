e5c3fd085b9ffe3e506087e39c0c336eb3d9d6ef
Igor Santos <falaigors@gmail.com>
2022-10-19 13:20:26 -0300

    create profile component in HomePage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,27 @@
+import {Header} from "../../components/Header";
+import {HomeContainer, ProfileWrapper} from './styles'
+
 export function Home() {
     return (
-        <div>Home Page</div>
+        <>
+            <Header/>
+            <HomeContainer>
+                <ProfileWrapper>
+                    <div>Imagem</div>
+                    <div>
+                        <div>
+                            <h2>Igor Santos</h2>
+                            <span>GITHUB</span>
+                        </div>
+                        <p>Um texto bem bacana sobre minha pessoa</p>
+                        <div>
+                            <span>github</span>
+                            <span>empresa</span>
+                            <span>seguidores</span>
+                        </div>
+                    </div>
+                </ProfileWrapper>
+            </HomeContainer>
+        </>
     )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,25 @@
 import styled from 'styled-components'
+
+export const HomeContainer = styled.main`
+max-width: 54rem;
+width: 100%;
+display: flex;
+justify-content:center;
+align-items:center;
+margin: 0 auto;
+
+`
+
+export const ProfileWrapper = styled.div`
+max-width: 54rem;
+width: 100%;
+margin-top: -4rem;
+
+padding: 2rem 2.5rem;
+border-radius: 8px;
+
+display: flex;
+justify-content: space-between;
+
+background-color: ${props => props.theme["base-profile"]};
+`
