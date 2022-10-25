8425c0640a4228f29300d7ee9b0c740a6f015db9
Igor Santos <falaigors@gmail.com>
2022-10-24 21:13:57 -0300

    get user profile to github

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,21 +1,69 @@
-import {ProfileContainer} from './styles'
+import { useState, useCallback, useEffect } from "react";
+import {
+  ProfileContainer,
+  ProfileInfo,
+  ProfileInfoTitle,
+  ProfileSocial,
+} from "./styles";
+import axios from "axios";
+import { Buildings, GithubLogo, Share, Users } from "phosphor-react";
+
+interface UserProps {
+  avatar_url: string;
+  company: string;
+  bio: string;
+  name: string;
+  login: string;
+  html_url: string;
+  twitter_username: string;
+  followers: number;
+  following: number;
+}
 
 export function Profile() {
-    return (
-            <ProfileContainer>
-                <div>Imagem</div>
-                <div>
-                    <div>
-                        <h2>Igor Santos</h2>
-                        <span>GITHUB</span>
-                    </div>
-                    <p>Um texto bem bacana sobre minha pessoa</p>
-                    <div>
-                        <span>github</span>
-                        <span>empresa</span>
-                        <span>seguidores</span>
-                    </div>
-                </div>
-            </ProfileContainer>
-    )
+  const [user, setUser] = useState<UserProps>();
+
+  const fetchUser = useCallback(async (query?: string) => {
+    try {
+      const response = await axios.get(import.meta.env.VITE_GITHUB_USER_API);
+
+      setUser(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  console.log(user);
+
+  return (
+    <ProfileContainer>
+      <div>
+        <img src={user?.avatar_url} alt={user?.name} />
+      </div>
+      <ProfileInfo>
+        <ProfileInfoTitle>
+          <h2>{user?.name}</h2>
+          <a href={user?.html_url}>
+            GITHUB <Share />
+          </a>
+        </ProfileInfoTitle>
+        <p>{user?.bio}</p>
+        <ProfileSocial>
+          <span>
+            <GithubLogo size={20} weight="fill" /> {user?.login}
+          </span>
+          <span>
+            <Buildings size={20} weight="fill" /> {user?.company}
+          </span>
+          <span>
+            <Users size={20} weight="fill" /> {user?.followers} seguidores
+          </span>
+        </ProfileSocial>
+      </ProfileInfo>
+    </ProfileContainer>
+  );
 }
--- src/components/Profile/styles.ts
+++ src/components/Profile/styles.ts
@@ -1,15 +1,59 @@
 import styled from 'styled-components'
 
 export const ProfileContainer = styled.div`
-max-width: 54rem;
-width: 100%;
-margin-top: -4rem;
+  width: 100%;
+  max-width: 54rem;
+  margin-top: -4rem;
+  gap: 2rem;
 
-padding: 2rem 2.5rem;
-border-radius: 8px;
+  padding: 2rem 2.5rem;
+  border-radius: 8px;
 
-display: flex;
-justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 
-background-color: ${props => props.theme["base-profile"]};
-`
+  filter: drop-shadow(0 1rem 0.75rem ${(props) => props.theme["base-input"]});
+  background-color: ${(props) => props.theme["base-profile"]};
+
+  img {
+    width: 150px;
+    border-radius: 8px;
+  }
+`;
+
+export const ProfileInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+
+export const ProfileInfoTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  a {
+    display: flex;
+    align-items: center;
+
+    gap: 5px;
+
+    font-weight: 600;
+    text-decoration: none;
+    color: ${(props) => props.theme.blue};
+  }
+`;
+
+export const ProfileSocial = styled.div`
+  display: flex;
+  gap: 2rem;
+
+  span {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
+
+  svg {
+    color: ${(props) => props.theme["base-label"]};
+  }
+`;
